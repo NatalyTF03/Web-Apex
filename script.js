3c9cac4a8231e873436fedaac835bf5d0f112bae
NatalyTF03 <tafurfloresnataly2003@gmail.com>
2026-06-25 19:48:52 -0300

    subida inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+// Cambia el fondo del navbar al hacer scroll
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
